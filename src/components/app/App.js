3d891611412b2950b44404a60e6d877df97764cd
tatianaKonovalova <tatiana.konovalova24@gmail.com>
2022-02-19 20:52:24 +0200

    routes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import {Component} from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import FoodifyService from '../../FoodifyService';
 
 import Header from "../UI/header/Header";
